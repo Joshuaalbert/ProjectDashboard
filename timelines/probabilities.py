a7bd2886c238d6fc6513f96580845d7aeadd95cd
joshuaalbert <albert@strw.leidenuniv.nl>
2021-08-02 15:09:03 -0400

    modular attention

--- timelines/probabilities.py
+++ timelines/probabilities.py
@@ -29,5 +29,7 @@ def prod(l):
 
 def compute_event_probabilities(G, unused_num_samples):
     for n in nx.topological_sort(G):
+        if n.startswith("role_"):
+            continue
         G.nodes[n]['start_prob'] = prod([G.nodes[j]['success'] for j in G.predecessors(n)])
         G.nodes[n]['success'] = G.nodes[n]['success_prob']/100. * G.nodes[n]['start_prob']
