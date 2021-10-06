0eabb283f42165431866dac523779bd6b1ddf52c
Joshua G. Albert <albert@strw.leidenuniv.nl>
2021-10-06 10:36:39 -0400

    v2.0

--- timelines/processes.py
+++ timelines/processes.py
@@ -292,11 +292,11 @@ def render_processes(data, save_file, advanced, date_of_change):
 
     # Display them
     with st.expander("Processes"):
-        display_process = st.multiselect("Filter process", data['processes'])
+        # display_process = st.multiselect("Filter process", data['processes'])
         G, critical_path = get_critical_path(Cache(data), date_of_change)
         for process in nx.algorithms.topological_sort(G):
-            if (process not in display_process) and (len(display_process) > 0):
-                continue
+            # if (process not in display_process) and (len(display_process) > 0):
+            #     continue
             last_date = data['processes'][process]['last_date']
             _done = data['processes'][process]['history'][last_date]['done']
             _done_date = datetime.datetime.fromisoformat(data['processes'][process]['history'][last_date]['done_date'])
