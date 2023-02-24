946a8018d831729b36969aba063239b2fc477728
Joshua George Albert <albert@strw.leidenuniv.nl>
2023-02-23 21:22:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import streamlit as st
-from git_work.git_work import render_data
 from timelines.components import render_components
 
 
 
 def main():
     render_components()
-
-    # which = st.sidebar.radio("Choose view: ", ['Project Planner', 'Operations Report'], index=0,help='Choose which view to show.')
-    #
-    # if which == 'Project Planner':
-    #     render_components()
-    # elif which == 'Operations Report':
-    #     render_data()
-
 
 
 if __name__ == '__main__':
